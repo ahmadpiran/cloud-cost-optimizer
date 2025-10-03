62433cb545c892346e7658afacd654dfe0cbd510
ahmadpiran <ahmad.piran@gmail.com>
2025-10-03 14:02:58 +0300

    edit main.tf to use data exports rather than cur

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,28 +2,28 @@ provider "aws" {
   region = us-east-1
 }
 
-# S3 Bucket for CUR
-resource "aws_s3_bucket" "cur_bucket" {
-    bucket = "cloud-cost-optimizer-cur"
+# S3 Bucket for AWS Data Exports
+resource "aws_s3_bucket" "data_exports_bucket" {
+    bucket = "cloud-cost-optimizer-data-exports"
 }
 
-resource "aws_s3_bucket_acl" "cur_bucket_acl" {
-    bucket = aws_s3_bucket.cur_bucket.id
+resource "aws_s3_bucket_acl" "data_exports_bucket_acl" {
+    bucket = aws_s3_bucket.data_exports_bucket.id
     acl    = "private"
 }
 
-resource "aws_s3_bucket_versioning" "cur_bucket_versioning" {
-    bucket = aws_s3_bucket.cur_bucket.id
+resource "aws_s3_bucket_versioning" "data_exports_bucket_versioning" {
+    bucket = aws_s3_bucket.data_exports_bucket.id
     versioning_configuration {
         status = "Enabled"
     }
 }
 
-resource "aws_s3_bucket_lifecycle_configuration" "cur_bucket_lifecycle" {
-    bucket = aws_s3_bucket.cur_bucket.id
+resource "aws_s3_bucket_lifecycle_configuration" "data_exports_bucket_lifecycle" {
+    bucket = aws_s3_bucket.data_exports_bucket.id
 
     rule {
-        id    = "cur-lifecycle-rule"
+        id    = "data-exports-bucket-lifecycle-rule"
         status = "Enabled"
 
         expiration {
